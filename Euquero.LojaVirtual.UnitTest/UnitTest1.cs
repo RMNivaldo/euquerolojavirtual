f67c3e51de6db49e5afd330a9ea0c40e135f5909
Nivaldo Rocha <rocha.m.nivaldo@gmail.com>
2016-08-15 17:52:54 +0200

    initial commit

---
+++ Euquero.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Euquero.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestEuquero
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
